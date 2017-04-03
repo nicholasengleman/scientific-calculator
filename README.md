b81baaa643642f114fcdb26dddfd67c24bbd5e0f
Nicholas Engleman <nicholas@englemaninvest.com>
2017-04-03 19:26:04 -0400

    add input from keys

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This calculator was inspired by the Windows 10 Scientific Calator with the fold-out history view. This view appears when you expand the Windows 10 calculator horizontally. I wanted a skeumorphic design so I made the calculator window tall and added 4 lines of history at the bottom. 
 
-##Featues to add
+##Features to add
 
 1. Operators
 2. Input from keys
--- calculator.js
+++ calculator.js
@@ -6,8 +6,8 @@ var model = {
     addInput: function (input) {
         this.displayCurrent.push(input);
         render.displayInput();
-    },
-}
+    }
+};
 
 
 //View
@@ -15,15 +15,15 @@ var render = {
     displayInput: function () {
         var displayLine1 = document.getElementById("displayLine1");
         displayLine1.innerHTML = "";
-        model.displayCurrent.forEach(function(input) {
-            if(render.translateOperator(input) == input) {
+        model.displayCurrent.forEach(function (input) {
+            if (render.translateOperator(input) === input) {
                 displayLine1.innerHTML += render.translateOperator(input);
             } else {
-                displayLine1.innerHTML += " " + render.translateOperator(input) + " ";
+                displayLine1.innerHTML += " <h3>" + render.translateOperator(input) + "</h3> ";
             }
-            
+
         });
-       this.dynamicFontSize(); 
+        this.dynamicFontSize();
     },
 
     translateOperator: function (input) {
@@ -41,17 +41,16 @@ var render = {
         }
     },
 
-    dynamicFontSize: function() {
+    dynamicFontSize: function () {
         //reduces font-size by 90% at a time till the equation fits inside the display's width
-        while(document.getElementById("displayLine1").clientWidth > 390) {
+        while (document.getElementById("displayLine1").clientWidth > 390) {
             var displayLine1 = document.getElementById("displayLine1");
-            var FontSize = parseFloat(window.getComputedStyle(displayLine1, null).getPropertyValue("font-size"));
-            displayLine1.style.fontSize = (FontSize - 1).toString() + "px";
+            var currentFontSize = parseFloat(window.getComputedStyle(displayLine1, null).getPropertyValue("font-size"));
+            displayLine1.style.fontSize = (currentFontSize - 1).toString() + "px";
 
         }
     }
-}
-
+};
 
 
 //Controller
@@ -61,6 +60,50 @@ var handlers = {
             model.addInput(e.target.parentNode.id);
         } else {
             model.addInput(e.target.id);
+
+
+        }
+    },
+
+    findKey: function (e) {
+        if (e.keyCode === 49 || e.keyCode === 97) {
+            model.addInput(1);
+        }
+        if (e.keyCode === 50 || e.keyCode === 98) {
+            model.addInput(2);
+        }
+        if (e.keyCode === 51 || e.keyCode === 99) {
+            model.addInput(3);
+        }
+        if (e.keyCode === 52 || e.keyCode === 100) {
+            model.addInput(4);
+        }
+        if (e.keyCode === 53 || e.keyCode === 101) {
+            model.addInput(5);
+        }
+        if (e.keyCode === 54 || e.keyCode === 102) {
+            model.addInput(6);
+        }
+        if (e.keyCode === 55 || e.keyCode === 103) {
+            model.addInput(7);
+        }
+        if (e.keyCode === 56 || e.keyCode === 104) {
+            model.addInput(8);
+        }
+        if (e.keyCode === 57 || e.keyCode === 105) {
+            model.addInput(9);
+        }
+        if (e.keyCode === 107) {
+            model.addInput("plus");
+        }
+        if (e.keyCode === 109) {
+            model.addInput("minus");
+        }
+        if (e.keyCode === 186 || e.keyCode === 106) {
+            model.addInput("times");
+        }
+        if (e.keyCode === 191 || e.keyCode === 111) {
+            model.addInput("divide");
         }
     }
-}
+};

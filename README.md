deee140070a9869ef6ff896ea89992339f108437
Nicholas Engleman <nicholas@englemaninvest.com>
2017-04-04 23:33:46 -0400

    add support for more operations and re-arranged keys

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # calculator
 
-This calculator was inspired by the Windows 10 Scientific Calator with the fold-out history view. This view appears when you expand the Windows 10 calculator horizontally. I wanted a skeumorphic design so I made the calculator window tall and added 4 lines of history at the bottom. 
+This calculator was inspired by the Windows 10 Scientific Calculator with the fold-out history view. This view appears when you expand the Windows 10 calculator horizontally. I wanted a skeumorphic design so I made the calculator window tall and added 4 lines of history at the bottom.
 
 ##Features to add
 
 1. Operators
-2. Input from keys
+
 
 
--- calculator.js
+++ calculator.js
@@ -1,3 +1,5 @@
+"use strict";
+
 //Model
 var model = {
     displayCurrent: [],
@@ -6,8 +8,59 @@ var model = {
     addInput: function (input) {
         this.displayCurrent.push(input);
         render.displayInput();
+    },
+
+    calc: function () {
+        var i;
+        //computes exponents and roots
+        for (i = 0; i < this.displayCurrent.length; i++) {
+            switch (this.displayCurrent[i]) {
+                case "xToTheNegativeOne":
+                    this.displayCurrent.splice(i - 1, 2, Math.pow(parseFloat(this.displayCurrent[i - 1]), -1));
+                    i--;
+                    break;
+                case "xsquared":
+                    this.displayCurrent.splice(i - 1, 2, Math.pow(parseFloat(this.displayCurrent[i - 1]), 2 ));
+                    i--;
+                    break;
+            }
+        }
+        //computes multiplication and division
+        for (i = 0; i < this.displayCurrent.length; i++) {
+            switch (this.displayCurrent[i]) {
+                case "divide":
+                    this.displayCurrent.splice(i - 1, 3, parseFloat(this.displayCurrent[i - 1]) / parseInt(this.displayCurrent[i + 1]));
+                    i--;
+                    break;
+                case "times":
+                    this.displayCurrent.splice(i - 1, 3, parseFloat(this.displayCurrent[i - 1]) * parseFloat(this.displayCurrent[i + 1]));
+                    i--;
+                    break;
+            }
+        }
+
+        //computes addition and subtraction
+        for (i = 0; i < this.displayCurrent.length; i++) {
+            switch (this.displayCurrent[i]) {
+                case "plus":
+                    this.displayCurrent.splice(i - 1, 3, parseFloat(this.displayCurrent[i - 1]) + parseFloat(this.displayCurrent[i + 1]));
+                    i--;
+                    break;
+                case "minus":
+                    this.displayCurrent.splice(i - 1, 3, parseFloat(this.displayCurrent[i - 1]) - parseFloat(this.displayCurrent[i + 1]));
+                    i--;
+                    break;
+            }
+        }
+        render.displayInput();
+    },
+
+    clear: function() {
+       this.displayCurrent = [];
+       render.displayInput();
     }
-};
+}
+
 
 
 //View
@@ -18,12 +71,14 @@ var render = {
         model.displayCurrent.forEach(function (input) {
             if (render.translateOperator(input) === input) {
                 displayLine1.innerHTML += render.translateOperator(input);
+            }  else if(render.translateOperator(input).search("sup") !== -1) {
+                displayLine1.innerHTML += " " + render.translateOperator(input) + " ";
             } else {
                 displayLine1.innerHTML += " <h3>" + render.translateOperator(input) + "</h3> ";
             }
-
         });
         this.dynamicFontSize();
+        console.log(model.displayCurrent);
     },
 
     translateOperator: function (input) {
@@ -36,13 +91,17 @@ var render = {
                 return "-";
             case 'plus':
                 return "+";
+            case 'xToTheNegativeOne':
+                return "<sup>-1</sup>";
+            case 'xsquared':
+                return "<sup>2</sup>";
             default:
                 return input;
         }
     },
 
     dynamicFontSize: function () {
-        //reduces font-size by 90% at a time till the equation fits inside the display's width
+        //reduces font-size so that the input always fits inside the display's width
         while (document.getElementById("displayLine1").clientWidth > 390) {
             var displayLine1 = document.getElementById("displayLine1");
             var currentFontSize = parseFloat(window.getComputedStyle(displayLine1, null).getPropertyValue("font-size"));
@@ -50,18 +109,25 @@ var render = {
 
         }
     }
-};
-
+}
 
 //Controller
 var handlers = {
     findId: function (e) {
         if (!e.target.id) {
-            model.addInput(e.target.parentNode.id);
+            if (e.target.parentNode.id === "enter") {
+                model.calc();
+            } else if (e.target.parentNode.id === "clear") {
+               model.clear();
+            } else {
+                model.addInput(e.target.parentNode.id);
+            }
+        } else if (e.target.id === "enter") {
+            model.calc();
+        } else if(e.target.id === "clear") {
+            model.clear();
         } else {
             model.addInput(e.target.id);
-
-
         }
     },
 

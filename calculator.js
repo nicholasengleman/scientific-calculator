63de53cf52bec7ef5b23d2577c8095ef1bef5e8c
Nicholas Engleman <nicholas@englemaninvest.com>
2017-04-11 13:45:34 -0400

    fixed bug in how numbers with multiple digits were stored

--- calculator.js
+++ calculator.js
@@ -3,10 +3,22 @@ var model = {
     displayCurrent: [],
     displayHistory: [],
     equation: "",
+    lastInputNumber: false,
 
-    addInput: function (input) {
-        this.displayCurrent.push(input);
+    addInput: function (input, isNumber) {
+        if (isNumber && this.lastInputNumber) {
+            this.displayCurrent[this.displayCurrent.length - 1] = this.displayCurrent[this.displayCurrent.length - 1] + input;
+        } else {
+            this.displayCurrent.push(input);
+        }
+        if (isNumber) {
+            this.lastInputNumber = true;
+        }
+        if (!isNumber) {
+            this.lastInputNumber = false;
+        }
         render.displayInput();
+        console.log(this.displayCurrent);
     },
 
     calc: function () {
@@ -30,6 +42,10 @@ var model = {
                     break;
                 case "xsquared":
                     this.displayCurrent.splice(i - 1, 2, Math.pow(parseFloat(this.displayCurrent[i - 1]), 2));
+                    i--;
+                    break;
+                case "power":
+                    this.displayCurrent.splice(i - 1, 3, Math.pow(parseFloat(this.displayCurrent[i - 1]), parseFloat(this.displayCurrent[i + 1])));
                     i--;
                     break;
             }
@@ -68,8 +84,8 @@ var model = {
 
     clear: function (clearAllHistory) {
         this.displayCurrent = [];
-        if(clearAllHistory) {
-            for(var i = 2; i<8; i++) {
+        if (clearAllHistory) {
+            for (var i = 2; i < 8; i++) {
                 $("#displayLine" + (i)).html("");
             }
         }
@@ -100,6 +116,8 @@ var render = {
                 displayLine1.innerHTML += render.translateOperator(input);
             } else if (render.translateOperator(input).search("sup") !== -1) {
                 displayLine1.innerHTML += " " + render.translateOperator(input) + " ";
+            } else if (render.translateOperator(input).search("&radic;") !== -1) {
+                displayLine1.innerHTML += render.translateOperator(input);
             } else {
                 displayLine1.innerHTML += " <h3>" + render.translateOperator(input) + "</h3> ";
             }
@@ -123,6 +141,12 @@ var render = {
                 return "<sup>2</sup>";
             case "sine":
                 return "sin(";
+            case "power":
+                return "^";
+            case "squareroot":
+                return "&radic;(";
+            case "rightParen":
+                return ")";
             default:
                 return input;
         }
@@ -130,71 +154,83 @@ var render = {
 
     dynamicFontSize: function () {
         //reduces font-size so that the input always fits inside the display's width
-        while (document.getElementById("displayLine1").clientWidth > 390) {
-            var displayLine1 = document.getElementById("displayLine1");
-            var currentFontSize = parseFloat(window.getComputedStyle(displayLine1, null).getPropertyValue("font-size"));
-            displayLine1.style.fontSize = (currentFontSize - 1).toString() + "px";
-
+        for (var i = 1; i < 3; i++) {
+            while (document.getElementById("displayLine"+i).clientWidth > 390) {
+                var displayLine = document.getElementById("displayLine"+1);
+                var currentFontSize = parseFloat(window.getComputedStyle(displayLine, null).getPropertyValue("font-size"));
+                displayLine.style.fontSize = (currentFontSize - 1).toString() + "px";
+            }
         }
     }
 };
 
 //Controller
 var handlers = {
-    findId: function (e) {
-        if (e.target.id === "solve") {
-            model.calc();
-        } else if (e.target.id === "clear") {
-            model.clear(false);
-        } else if (e.target.id === "clearhistory") {
-            model.clear(true);
-        } else if (e.target.id) {
-            model.addInput(e.target.id);
-        } else if (e.target.parentNode.id) {
-            model.addInput(e.target.parentNode.id);
-        }
-    },
+        findId: function (e) {
+            if (e.target.id === "solve") {
+                model.calc();
+            } else if (e.target.id === "clear") {
+                model.clear(false);
+            } else if (e.target.id === "clearhistory") {
+                model.clear(true);
 
-    findKey: function (e) {
-        if (e.keyCode === 49 || e.keyCode === 97) {
-            model.addInput(1);
-        }
-        if (e.keyCode === 50 || e.keyCode === 98) {
-            model.addInput(2);
-        }
-        if (e.keyCode === 51 || e.keyCode === 99) {
-            model.addInput(3);
-        }
-        if (e.keyCode === 52 || e.keyCode === 100) {
-            model.addInput(4);
-        }
-        if (e.keyCode === 53 || e.keyCode === 101) {
-            model.addInput(5);
-        }
-        if (e.keyCode === 54 || e.keyCode === 102) {
-            model.addInput(6);
-        }
-        if (e.keyCode === 55 || e.keyCode === 103) {
-            model.addInput(7);
-        }
-        if (e.keyCode === 56 || e.keyCode === 104) {
-            model.addInput(8);
-        }
-        if (e.keyCode === 57 || e.keyCode === 105) {
-            model.addInput(9);
-        }
-        if (e.keyCode === 107) {
-            model.addInput("plus");
-        }
-        if (e.keyCode === 109) {
-            model.addInput("minus");
-        }
-        if (e.keyCode === 186 || e.keyCode === 106) {
-            model.addInput("times");
-        }
-        if (e.keyCode === 191 || e.keyCode === 111) {
-            model.addInput("divide");
+            } else if (e.target.id) {
+                if (!isNaN(e.target.id)) {
+                    model.addInput(e.target.id, true);
+                } else {
+                    model.addInput(e.target.id, false);
+                }
+            }
+            else if (e.target.parentNode.id) {
+                if (!isNaN(e.target.parentNode.id)) {
+                    model.addInput(e.target.parentNode.id, true);
+                } else {
+                    model.addInput(e.target.parentNode.id, false);
+                }
+            }
+        },
+
+        findKey: function (e) {
+            if (e.keyCode === 49 || e.keyCode === 97) {
+                model.addInput(1, true);
+            }
+            if (e.keyCode === 50 || e.keyCode === 98) {
+                model.addInput(2, true);
+            }
+            if (e.keyCode === 51 || e.keyCode === 99) {
+                model.addInput(3, true);
+            }
+            if (e.keyCode === 52 || e.keyCode === 100) {
+                model.addInput(4, true);
+            }
+            if (e.keyCode === 53 || e.keyCode === 101) {
+                model.addInput(5, true);
+            }
+            if (e.keyCode === 54 || e.keyCode === 102) {
+                model.addInput(6, true);
+            }
+            if (e.keyCode === 55 || e.keyCode === 103) {
+                model.addInput(7, true);
+            }
+            if (e.keyCode === 56 || e.keyCode === 104) {
+                model.addInput(8, true);
+            }
+            if (e.keyCode === 57 || e.keyCode === 105) {
+                model.addInput(9, true);
+            }
+            if (e.keyCode === 107) {
+                model.addInput("plus", false);
+            }
+            if (e.keyCode === 109) {
+                model.addInput("minus", false);
+            }
+            if (e.keyCode === 186 || e.keyCode === 106) {
+                model.addInput("times", false);
+            }
+            if (e.keyCode === 191 || e.keyCode === 111) {
+                model.addInput("divide", false);
+            }
         }
     }
-};
+;
 
